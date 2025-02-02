2238d38f1c9d8442b04dcc132ba7424e8dc1a335
Billy Jo Ayakatubby <bhaelochon@gmail.com>
2025-02-02 13:44:54 -0600

    Change startup method
    Run each script individually instead of all together in one AHK instance

---
+++ AutoHotkeyStartup.cmd
@@ -0,0 +1,7 @@
+@REM
+@REM Distributed under the Unlicense <https://unlicense.org>
+@REM
+
+START .\Scripts\AutoclickTrigger.ahk
+START .\Scripts\CenterWindowTrigger.ahk
+START .\Scripts\HappyDayTrigger.ahk
